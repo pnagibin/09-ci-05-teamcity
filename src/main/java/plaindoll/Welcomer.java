73b147a10788857050ff2a3be1d3289f2b01de4c
Pavel Nagibin <pnagibin@gmail.com>
2023-11-14 14:03:46 +0300

    1

---
+++ src/main/java/plaindoll/Welcomer.java
@@ -0,0 +1,16 @@
+package plaindoll;
+
+public class Welcomer{
+	public String sayWelcome() {
+		return "Welcome home, good hunter. What is it your desire?";
+	}
+	public String sayFarewell() {
+		return "Farewell, good hunter. May you find your worth in waking world.";
+	}
+	public String sayStatus() {
+		return "Hello! My status is Good";
+	}
+	public String sayHunterwho() {
+	    return "Who is hunter?";
+	}
+}
